5b6573c06835a3b15cd09ed2def59b5a19582d1a
BOBDA DJIMO ARMEL HYACINTHE <armelhbobdad@gmail.com>
2022-08-22 22:38:21 +0400

    Handle todos in localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
@@ -6,9 +6,14 @@ import TodoList from './components/TodoList';
 
 function App() {
 
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(initialState);
   const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
   return (
     <div className="container">
