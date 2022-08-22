1aae28dfdccc6b98b4a74ad7e7d7935768d1ac65
BOBDA DJIMO ARMEL HYACINTHE <armelhbobdad@gmail.com>
2022-08-22 21:31:37 +0400

    Create todo action and check if it work on Components in chrome react debug mode.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="container">
       <div className='app-wrapper'>
@@ -10,7 +15,12 @@ function App() {
           <Header/>
         </div>
         <div>
-          <Form/>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+          />
         </div>
       </div>
     </div>
