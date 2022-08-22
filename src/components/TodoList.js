b314c441b595a0dfc2b0a307df1d8bd6bc9eacd0
BOBDA DJIMO ARMEL HYACINTHE <armelhbobdad@gmail.com>
2022-08-22 22:01:43 +0400

    handleComplete, handleDelete and fix css class completed.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 const TodoList = ({todos, setTodos}) => {
+
+    const handleComplete = (todo) => {
+        setTodos(
+            todos.map((item) => {
+                if(item.id === todo.id) {
+                    return {...item, completed: !item.completed}
+                }
+                return item;
+            })
+        )
+    }
+
+    const handleDelete = ({id}) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    }
+
   return (
     <div>
         {todos.map((todo) => (
@@ -8,17 +24,17 @@ const TodoList = ({todos, setTodos}) => {
                 <input
                     type="text"
                     value={todo.title}
-                    className="list"
+                    className={`list ${todo.completed ? "completed" : ""}`}
                     onChange={(event) => event.preventDefault()}
                 />
                 <div>
-                    <button className='button-complete task-button'>
+                    <button className='button-complete task-button' onClick={() => handleComplete(todo)}>
                         <i className='fa fa-check-circle'/>
                     </button>
                     <button className='button-edit task-button'>
                         <i className='fa fa-edit'/>
                     </button>
-                    <button className='button-delete task-button'>
+                    <button className='button-delete task-button' onClick={() => handleDelete(todo)}>
                         <i className='fa fa-trash'/>
                     </button>
                 </div>
